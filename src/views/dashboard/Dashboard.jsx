0eae9b48a632f81540b6aa30eae49f1a9069882f
Manav Kothari <manav.kothari@openxcellinc.com>
2020-09-25 14:59:29 +0530

    trello dashbaord

--- src/views/dashboard/Dashboard.jsx
+++ src/views/dashboard/Dashboard.jsx
@@ -1,8 +1,62 @@
 import React from 'react';
+import Board from 'react-trello'
 
 const Dashboard = () => {
+  const data = {
+    lanes: [
+      {
+        id: 'lane1',
+        title: 'Todo Tasks',
+        label: '3/3',
+        cards: [
+          {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false},
+          {id: 'Card2', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false},
+          {id: 'Card3', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
+        ]
+      },
+      {
+        id: 'lane2',
+        title: 'In Progress',
+        label: '3/3',
+        cards: [
+          {id: 'Card4', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false},
+          {id: 'Card5', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false},
+          {id: 'Card6', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
+        ]
+      },
+      {
+        id: 'lane3',
+        title: 'Completed',
+        label: '3/3',
+        cards: [
+          {id: 'Card7', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false},
+          {id: 'Card8', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false},
+          {id: 'Card9', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
+        ]
+      },
+      {
+        id: 'lane4',
+        title: 'On Hold',
+        label: '3/3',
+        cards: [
+          {id: 'Card10', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false},
+          {id: 'Card11', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false},
+          {id: 'Card12', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
+        ]
+      },
+      {
+        id: 'lane5',
+        title: 'Cancelled',
+        label: '1/1',
+        cards: [{id: 'Card13', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}]
+      }
+    ]
+  }
+
   return (
-      <div>Dashboard</div>
+    <React.Fragment>
+        <Board data={data} />
+    </React.Fragment>
   );
 };
 
